71e5adf3ae12c59a35dba26bdb45e184316babc5
sk0g <ani.mehta@mail.com>
2022-05-25 22:25:27 +1000

    Phase duration calculations

--- deviceControl.cpp
+++ deviceControl.cpp
@@ -6,8 +6,8 @@
 
 DeviceControl::DeviceControl()
 {
-    for (auto &[pinNumber, pinMode]: pinConfig) {
-        switch (pinMode) {
+    for ( auto &[pinNumber, pinMode]: pinConfig ) {
+        switch ( pinMode ) {
             case DIGITAL_OUTPUT:
                 gpio_init( pinNumber );
                 gpio_set_dir( pinNumber, GPIO_OUT );
@@ -23,12 +23,17 @@ DeviceControl::DeviceControl()
 
                 break;
 
-            case PWM_INPUT:
-                gpio_set_function( pinNumber, GPIO_FUNC_PWM );
+            case DIGITAL_PWM_INPUT:
+                gpio_init( pinNumber );
+                gpio_set_dir( pinNumber, GPIO_IN );
+
+                gpio_set_irq_enabled_with_callback(
+                    pinNumber, 0b1100, true, gpio_phaseDuration_callback );
 
                 break;
 
             case PWM_OUTPUT:
+                gpio_init( pinNumber );
                 gpio_set_function( pinNumber, GPIO_FUNC_PWM );
 
                 break;
@@ -45,12 +50,29 @@ void DeviceControl::gpio_callback( uint gpio, uint32_t event )
     notifyPinValueChange( gpio, isHighSignal );
 }
 
+void DeviceControl::gpio_phaseDuration_callback( uint gpio, uint32_t event )
+{
+    bool isHighSignal = event == 8;
+
+    if ( isHighSignal ) {
+        lastPhaseRisingAt[gpio] = time_us_64();
+    } else {
+        auto pulseDuration = (time_us_64() - DeviceControl::lastPhaseRisingAt[gpio]) / 1e6;
+
+        // maybe should avoid sending relatively unchanged phase durations
+        char message[20];
+        sprintf( message, "%02i^%0.3f", gpio, pulseDuration );
+
+        SerialCommunication::addMessageToWrite( message );
+    }
+}
+
 
 void DeviceControl::setGpioPinValue( uint pinNumber, bool value, bool notify )
 {
     gpio_put( pinNumber, value );
 
-    if (notify) notifyPinValueChange( pinNumber, value );
+    if ( notify ) notifyPinValueChange( pinNumber, value );
 }
 
 bool DeviceControl::getGpioPinValue( uint pinNumber )
@@ -67,3 +89,4 @@ void DeviceControl::notifyPinValueChange( uint pinNumber, bool value )
     );
     SerialCommunication::addMessageToWrite( message );
 }
+
